6077d466fa7320c0dc1995f297278f1bb2b5befa
Maciej Lopacinski <74767715+JuiceC1@users.noreply.github.com>
2022-03-01 15:11:08 +0000

    Add files via upload

---
+++ week1/Welcome.java
@@ -0,0 +1,18 @@
+//  This is the hello world program
+
+/* By Maciej
+*/
+
+public class Welcome
+{
+
+	public static void main(String[] args)
+	{
+
+		System.out.println("Welcome to Java Programming");
+	}//main
+
+
+}// class
+
+
---
+++ week1/print_println.java
@@ -0,0 +1,32 @@
+//  This is the hello world program
+
+/* By Maciej
+*/
+
+public class print_println
+{
+
+	public static void main(String[] args)
+	{
+
+		//println
+		System.out.println("To code ");
+		System.out.println("Or not to code");
+		System.out.println();
+		System.out.println();
+		System.out.println();
+
+
+		//print
+		System.out.print("To code ");
+		System.out.print("Or not to code");
+
+		//This will generate an erro:
+		//System.out.print()
+
+	}//main
+
+
+}// class
+
+
